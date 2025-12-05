14390f84994ca87c4aa0508f1058498a6d1e8b2e
badzlinaatika <badzlinaatika25@gmail.com>
2025-12-05 17:06:45 +0700

    Create repository_foldersRoutes.js

---
+++ Trackify_Back_End/src/routes/repository_foldersRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const repository_foldersController = require("../controllers/repository_foldersController");
+const authenticateToken = require('../middlewares/authMiddleware');
+
+// Create folder
+router.post("/", authenticateToken, repository_foldersController.createFolder);
+
+// Get all folders by user
+router.get("/user/:userId", authenticateToken, repository_foldersController.getFolders);
+
+// Update folder
+router.put("/:id", authenticateToken, repository_foldersController.updateFolder);
+
+// Delete folder (+ files)
+router.delete("/:id", authenticateToken, repository_foldersController.deleteFolder);
+
+module.exports = router;
