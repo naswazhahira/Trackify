c8c5d7cdba43ac2f802583de01225f9423f5fa7f
badzlinaatika <badzlinaatika25@gmail.com>
2025-12-06 21:50:20 +0700

    Update repository_filesRoutes.js

--- Trackify_Back_End/src/routes/repository_filesRoutes.js
+++ Trackify_Back_End/src/routes/repository_filesRoutes.js
@@ -4,15 +4,15 @@ const repository_filesController = require("../controllers/repository_filesContr
 const authenticateToken = require('../middlewares/authMiddleware');
 
 
-// Upload file
-router.post("/files", repository_filesController.uploadFile);
+// Upload file (POST /api/repository_files)
+router.post("/", authenticateToken, repository_filesController.uploadFile);
 
 
-// Get files in folder
-router.get("/files/folder/:folderId", repository_filesController.getFiles);
+// Get files in folder (GET /api/repository_files/:folderId)
+router.get("/:folderId", authenticateToken, repository_filesController.getFiles);
 
 
-// Delete single file
-router.delete("/files/:id", repository_filesController.deleteFile);
+// Delete single file (DELETE /api/repository_files/:id)
+router.delete("/:id", authenticateToken, repository_filesController.deleteFile);
 
 module.exports = router;
